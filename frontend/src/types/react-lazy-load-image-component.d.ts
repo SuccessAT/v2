66a2dfb61f7c7beff61a137f9e07061f9164b031
SuccessAT <agboyinusuccess@gmail.com>
2024-11-14 18:08:09 +0100

    Updates

--- frontend/src/types/react-lazy-load-image-component.d.ts
+++
@@ -1,8 +0,0 @@
-// src/react-lazy-load-image-component.d.ts
-declare module 'react-lazy-load-image-component' {
-  import { ComponentType } from 'react';
-
-  export const LazyLoadImage: ComponentType<any>;
-  export const LazyLoadComponent: ComponentType<any>;
-}
-
